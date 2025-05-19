3fafc877ed2932092610a0eeca5c0d7ba78c0b62
James Schallert <33075152+JamesSchallert@users.noreply.github.com>
2025-05-19 15:56:39 -0400

    Updating "hold to gather" functionality to simplify logic and fix a bug with infinite gathering.

--- core.js
+++ core.js
@@ -12,7 +12,7 @@ function gather(resource_name) {
 
 const GatherRateLimit = 90; // milliseconds between allowed gathers
 const GatherCooldowns = {};
-const GatherIntervals = {};
+let gather_interval = null;
 
 /* Create a list of strings of resource types, then iterate over them to
  initialize objects that hold the resource amounts, gather rates, ui
@@ -37,13 +37,16 @@ for (const resource_type of resource_types) {
   ResourceButtons[resource_type] = 
     document.getElementById(`gather-${resource_type}-button`);
 
-  // Note:  Due to closure, each iteration (resource) has its own canGather.
-  let canGather = true;
+  
 
   /* Function to limit clicking speed.  Autoclicker software often breaks
   these games.  After gathering, canGather is set to false.  An arrow
   function is then set to occur after a certain time (GatherRateLimit)
   that sets canGather back to true.*/
+
+  // Note:  Due to closure, each iteration (resource) has its own canGather.
+  let canGather = true;
+
   function tryGather() {
     if (canGather) {
       gather(resource_type);
@@ -61,12 +64,36 @@ for (const resource_type of resource_types) {
   addListeners(
     ResourceButtons[resource_type],
     ["mousedown","touchstart"],
-    () => {GatherIntervals[resource_type] = setInterval(tryGather, 50);},
+    () => {
+      if (!(gather_interval)) {
+        gather_interval = setInterval(tryGather, 50);
+      };
+    },
   );
   addListeners(
     ResourceButtons[resource_type],
-    ["mouseup","mouseleave","touchend","touchcancel"],
-    () => {clearInterval(GatherIntervals[resource_type]);},
-  );
+    ["mouseup","mouseleave","touchend","touchcancel","contextmenu",],
+    () => {
+      clearInterval(gather_interval);
+      gather_interval = null;
+    },
+  );  
 };
 
+// Clear any gather_interval running if the window loses focus or any other
+// scenario that makes sense.  A sort of catch-all to clean up gather_interval.
+addListeners(
+  window,
+  ["mouseup","mouseleave","touchend","touchcancel","contextmenu","blur"],
+  () => {
+    clearInterval(gather_interval);
+    gather_interval = null;
+  },
+);
+
+//debug code, can remove later, but useful to have ready to go.
+// window.addEventListener("blur", () => console.log("Window blurred"));
+// window.addEventListener("contextmenu", () => 
+//   console.log("Context menu fired"));
+// window.addEventListener("mouseup", () => console.log("Mouse up"));
+// window.addEventListener("mousedown", () => console.log("Mousedown"));
