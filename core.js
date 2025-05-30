f0cdcca3b466a849a7e9217cac4cb59ca7c520d0
James Schallert <33075152+JamesSchallert@users.noreply.github.com>
2025-05-30 13:51:54 -0400

    Moving tryGather function and canGather from encapsulated per resource to global.  tryGather now accepts a resource_type as an argument.

--- core.js
+++ core.js
@@ -4,15 +4,31 @@ import { addListeners } from './utils.js';
 // A function that takes a resource name string as the argument and updates
 // that resource's value in the resource amounts object by its value in the
 // gather rates object.  It then updates the counter that the player sees.
-function gather(resource_name) {
-    ResourceAmounts[resource_name] += GatherRates[resource_name];
-    ResourceCounters[resource_name].innerText = 
-      `${ResourceAmounts[resource_name]}`;
+function gather(resource_type) {
+    ResourceAmounts[resource_type] += GatherRates[resource_type];
+    ResourceCounters[resource_type].innerText = 
+      `${ResourceAmounts[resource_type]}`;
+};
+
+/* Function to limit clicking speed.  Autoclicker software often breaks
+  these games.  Before gathering, global canGather is set to false.  An arrow
+  function is then set to occur after a certain time (GatherRateLimit)
+  that sets canGather back to true.*/
+function tryGather(resource_type) {
+    if (canGather) {
+      canGather = false;
+      gather(resource_type);
+      setTimeout(
+        () => { canGather = true; }, 
+        GatherRateLimit
+      );
+    };
 };
 
 const GatherRateLimit = 90; // milliseconds between allowed gathers
 const GatherCooldowns = {};
 let gather_interval = null;
+let canGather = true;
 
 /* Create a list of strings of resource types, then iterate over them to
  initialize objects that hold the resource amounts, gather rates, ui
@@ -37,27 +53,11 @@ for (const resource_type of resource_types) {
   ResourceButtons[resource_type] = 
     document.getElementById(`gather-${resource_type}-button`);
 
-  
-
-  /* Function to limit clicking speed.  Autoclicker software often breaks
-  these games.  After gathering, canGather is set to false.  An arrow
-  function is then set to occur after a certain time (GatherRateLimit)
-  that sets canGather back to true.*/
-
-  // Note:  Due to closure, each iteration (resource) has its own canGather.
-  let canGather = true;
-
-  function tryGather() {
-    if (canGather) {
-      gather(resource_type);
-      canGather = false;
-      setTimeout(
-        () => { canGather = true; }, 
-        GatherRateLimit
-      );
-    };
-  };
-  ResourceButtons[resource_type].addEventListener("click", () => tryGather());
+  /* Add ability to gather resources by clicking */ 
+  ResourceButtons[resource_type].addEventListener(
+    "click",
+    () => tryGather(resource_type)
+  );
 
   /* Add ability to gather by holding down click or touch by setting an interval
     to tryGather on holding, then clearing the interval when releasing. */
